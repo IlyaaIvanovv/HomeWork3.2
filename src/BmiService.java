b2c11fff0113a8477798d548db210b25dbebd3c4
Ilya Ivanov <ilyaivanov101@yandex.ru>
2023-02-14 16:27:13 +0300

    Fix ignore

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,8 @@
 public class BmiService {
+
+    public int calculate (double high, int weight){
+        int index;
+        index = (int) (weight / (high * high));
+        return index;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+    BmiService service = new BmiService();
+    double high = 1.65;
+    int weight = 50;
+    int index = service.calculate(high, weight);
+    System.out.println("Ваш BMI-индекс: " + (index));
+    }
 }
